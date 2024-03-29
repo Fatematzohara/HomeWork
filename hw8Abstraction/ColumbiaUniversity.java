a73106cd424191163c5303326bb7722bf768ffca
Fatematzohara <fatematuz.qa@gmail.com>
2024-03-28 23:28:13 -0400

    abstract 3-28

--- hw8Abstraction/ColumbiaUniversity.java
+++ hw8Abstraction/ColumbiaUniversity.java
@@ -3,15 +3,15 @@ package hw8Abstraction;
 public class ColumbiaUniversity {
 
 	// public abstract void chemistry() ;
-	// method can be implemented inside a class, but not declared or defined
+	// method can be implemented inside a regular class, but not declared or defined
 
 	public void biology() {
 		System.out.println("this is from non abstract method biology");
 	}
 
-//	public default void cons() {
-//	
-//		System.out.println("This is all from default constructor");
-//	}
-	// Default constructors are allowed only in interfaces, not in regular class
+	public ColumbiaUniversity() {
+
+		System.out.println("This is all from default constructor");
+	}
+	// Default constructors are allowed in regular class
 }
