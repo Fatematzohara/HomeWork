9355687193663fa841d1d60ebe66d727d22176c4
Fatematzohara <fatematuz.qa@gmail.com>
2024-05-31 00:08:23 -0400

    Selenium methods

---
+++ homeWork/Selenium.java
@@ -0,0 +1,21 @@
+package homeWork;
+
+public class Selenium {
+
+//	a) Tag with a single attribute
+
+//	Ans. //div[@class='flex-child']
+
+//	b) Tag with multiple attributes with 'and' logic
+
+// Ans. //a[@id='header_order_and_returns' and @class='myaccount']
+
+//	c) Tag with inner text
+
+//	Ans. //a[text()='Orders & Returns']
+
+//	d) Tag with partial inner text from any website
+
+//Ans.  //a[contains( text(),'Orders & R')]
+
+}
